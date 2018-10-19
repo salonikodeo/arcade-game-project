ab684c4a18b48b67dcead7b44e46c86ba1696df9
Saloni <saloni08g@gmail.com>
2018-10-19 22:44:04 +0530

    Update enemy class with ES6 code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,33 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    constructor(x,y,speed) {
+        // set position for enemy
+        this.x = x;
+        this.y = y;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        //set speed for enemy
+        this.speed = speed;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+    // Draw the enemy on the screen, required method for game
+    render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -46,7 +54,7 @@ class Player {
 
     //Handle the input provided by the user
     handleInput() {
-        
+
     }
 }
 
